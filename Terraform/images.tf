97bf9e2fc5f418237bd662ef02c4c9ad71d7e8ff
Jose Garcia <joseantonio64211@gmail.com>
2025-08-11 11:31:49 -0600

    added truenas scale ISO and VM

--- Terraform/images.tf
+++ Terraform/images.tf
@@ -15,3 +15,13 @@ resource "harvester_image" "ubuntu20" {
   source_type        = "download"
   url                = "http://cloud-images.ubuntu.com/releases/focal/release/ubuntu-20.04-server-cloudimg-amd64.img"
 }
+
+
+resource "harvester_image" "truenas-scale" {
+  name               = "truenas-scale"
+  namespace          = "harvester-public"
+  storage_class_name = "harvester-longhorn"
+  display_name       = "TrueNAS-SCALE-25.04.2.1.iso"
+  source_type        = "download"
+  url                = "https://download.sys.truenas.net/TrueNAS-SCALE-Fangtooth/25.04.2.1/TrueNAS-SCALE-25.04.2.1.iso"
+}
