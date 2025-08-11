f0081d7855e8500cf5f224d188c9a38498c89b33
Jose Garcia <joseantonio64211@gmail.com>
2025-08-11 14:09:59 -0600

    Added minio S3 credentials for Terrraform Backend

---
+++ Terraform/backend.tf
@@ -0,0 +1,15 @@
+terraform {
+    backend "s3" {
+        bucket = "terraform"                  # Name of the S3 bucket
+        endpoints = {
+            s3 = "http://192.168.1.30:9000"   # Minio endpoint
+        }
+        key = "harvester.tfstate"        # Name of the tfstate file
+        region = "local"                     # Region validation will be skipped
+        skip_region_validation = true
+        skip_credentials_validation = true  # Skip AWS related checks and validations
+        skip_requesting_account_id = true
+        skip_metadata_api_check = true        
+        use_path_style = true             # Enable path-style S3 URLs (https://<HOST>/<BUCKET> https://developer.hashicorp.com/terraform/language/settings/backends/s3#use_path_style
+    }
+}
