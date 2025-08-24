03e023d88da746e1d97a3323ca622e491b4988af
Jose Garcia <jose@lanfordlabs.com>
2025-08-23 23:35:39 -0600

    renamed workflow to reference its jobs cloudflare

--- cloudflare/nginx.tf
+++ cloudflare/nginx.tf
@@ -1,18 +1,18 @@
 locals {
   domain = "lanfordlabs.com"
   proxy_hosts = [
-    { name = "gitlab",          ip = "192.168.1.6", forward_scheme = "http", subdomain = "gitlab", forward_port = 10080 },
-    { name = "s3-gui",          ip = "192.168.1.16", forward_scheme = "http", subdomain = "s3-gui", forward_port = 9002 },
-    { name = "s3",              ip = "192.168.1.16", forward_scheme = "http", subdomain = "s3", forward_port = 9000 },
-    { name = "grafana",         ip = "192.168.1.7", forward_scheme = "http", subdomain = "grafana", forward_port = 3030 },
-    { name = "home-assistant",  ip = "192.168.1.3", forward_scheme = "http", subdomain = "home-assistant", forward_port = 8123 },
-    { name = "pve",             ip = "192.168.1.24", forward_scheme = "https", subdomain = "pve", forward_port = 8006 },
-    { name = "harvester",       ip = "192.168.2.27", forward_scheme = "https", subdomain = "harvester", forward_port = 443 },
-    { name = "rancher",         ip = "192.168.2.27", forward_scheme = "https", subdomain = "rancher", forward_port = 443 },
-    { name = "truenas",         ip = "192.168.1.16", forward_scheme = "https", subdomain = "truenas", forward_port = 443 },
-    { name = "immich",          ip = "192.168.1.8", forward_scheme = "http", subdomain = "immich", forward_port = 2283 },
-    { name = "jellyfin",        ip = "192.168.1.59", forward_scheme = "http", subdomain = "jellyfin", forward_port = 8096 },
-    { name = "ytd",             ip = "192.168.1.8", forward_scheme = "http", subdomain = "ytd", forward_port = 8000 }
+    { name = "gitlab",          ip = "192.168.1.6",   forward_scheme = "http",  subdomain = "gitlab",         forward_port = 10080 },
+    { name = "s3-gui",          ip = "192.168.1.16",  forward_scheme = "http",  subdomain = "s3-gui",         forward_port = 9002 },
+    { name = "s3",              ip = "192.168.1.16",  forward_scheme = "http",  subdomain = "s3",             forward_port = 9000 },
+    { name = "grafana",         ip = "192.168.1.7",   forward_scheme = "http",  subdomain = "grafana",        forward_port = 3030 },
+    { name = "home-assistant",  ip = "192.168.1.3",   forward_scheme = "http",  subdomain = "home-assistant", forward_port = 8123 },
+    { name = "pve",             ip = "192.168.1.24",  forward_scheme = "https", subdomain = "pve",            forward_port = 8006 },
+    { name = "harvester",       ip = "192.168.2.27",  forward_scheme = "https", subdomain = "harvester",      forward_port = 443 },
+    { name = "rancher",         ip = "192.168.2.27",  forward_scheme = "https", subdomain = "rancher",        forward_port = 443 },
+    { name = "truenas",         ip = "192.168.1.16",  forward_scheme = "https", subdomain = "truenas",        forward_port = 443 },
+    { name = "immich",          ip = "192.168.1.8",   forward_scheme = "http",  subdomain = "immich",         forward_port = 2283 },
+    { name = "jellyfin",        ip = "192.168.1.59",  forward_scheme = "http",  subdomain = "jellyfin",       forward_port = 8096 },
+    { name = "ytd",             ip = "192.168.1.8",   forward_scheme = "http",  subdomain = "ytd",            forward_port = 8000 }
   ]
   certificate_id = 7
 }
