18758a467f18f8ab0ab49a13f1869973a404edab
Jose Garcia <jose@lanfordlabs.com>
2025-08-23 20:14:14 -0600

    added Terraform for cloudflare

--- cloudflare/test/main.tf
+++ cloudflare/test/main.tf
@@ -3,16 +3,16 @@ locals {
   proxy_ip = "192.168.1.6"
 
   dns_records = [
-    { name = "harvester", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "rancher", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "truenas", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "grafana", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "home-assistant", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "immich", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "jellyfin", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "pve", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "s3", type = "A", value = local.proxy_ip, ttl = 1 },
-    { name = "s3-gui", type = "A", value = local.proxy_ip, ttl = 1 }
+    { name = "harvester",       type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "rancher",         type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "truenas",         type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "grafana",         type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "home-assistant",  type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "immich",          type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "jellyfin",        type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "pve",             type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "s3",              type = "A", value = local.proxy_ip, ttl = 1 },
+    { name = "s3-gui",          type = "A", value = local.proxy_ip, ttl = 1 }
   ]
 }
 
