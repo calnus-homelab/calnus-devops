21fee69ae37533a32c36a112ad494e04ad22585a
Jose Garcia <jose@lanfordlabs.com>
2025-08-23 23:09:25 -0600

    added pve domain to the nginx proxy

--- cloudflare/nginx.tf
+++ cloudflare/nginx.tf
@@ -6,9 +6,8 @@ locals {
     { name = "s3", ip = "192.168.1.16", forward_scheme = "http", subdomain = "s3", forward_port = 9000 },
     { name = "grafana", ip = "192.168.1.7", forward_scheme = "http", subdomain = "grafana", forward_port = 3030 },
     { name = "home-assistant", ip = "192.168.1.3", forward_scheme = "http", subdomain = "home-assistant", forward_port = 8123 },
-    { name = "pve", ip = "192.168.1.24", forward_scheme = "http", subdomain = "pve", forward_port = 8006 }
+    { name = "pve", ip = "192.168.1.24", forward_scheme = "https", subdomain = "pve", forward_port = 8006 }
   ]
-
   certificate_id = 7
 }
 resource "nginxproxymanager_proxy_host" "tracked" {
