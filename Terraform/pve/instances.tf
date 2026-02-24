ca3a7375e37579f720be1e7d07f5f69763d6f49c
Pepe Toño <joseantonio64211@gmail.com>
2026-02-24 16:51:29 +0000

    updated nvme config on the cloudinit to format the drives

--- Terraform/pve/instances.tf
+++ Terraform/pve/instances.tf
@@ -1,38 +1,14 @@
-locals {
-  default_worker_instance_type = "m5.2large"
-  cloud_image                  = module.images.ubuntu_24_04.id
-  hostpci_id                   = null
-  storage_size                 = 40
-  nodes = {
-    master-01 = {
-      ip            = "192.168.1.60"
-      instance_type = "t3.medium"
-      storage_size  = 50
-    }
-    worker-01 = {
-      ip         = "192.168.1.61"
-      hostpci_id = "0000:01:00"
-    }
-    worker-02 = {
-      ip         = "192.168.1.62"
-      hostpci_id = "0000:02:00"
-    }
-    worker-03 = {
-      ip         = "192.168.1.63"
-      hostpci_id = "0000:59:00"
-    }
-  }
-}
+
 module "K3s_instances" {
   source            = "../Modules/instances"
-  for_each          = local.nodes
-  node_name         = var.node_name
+  for_each          = local.cluster_nodes
+  node_name         = local.proxmox_defaults.node_name
   server_name       = each.key
   ip_address        = each.value.ip
-  ssh_public_key    = var.ssh_public_key
-  nvme_storage_pool = var.storage_pool
+  ssh_public_key    = lookup(each.value, "ssh_public_key", local.ssh_public_key)
+  nvme_storage_pool = lookup(each.value, "nvme_storage_pool", local.nvme_storage_pool)
   cloud_image       = lookup(each.value, "cloud_image", local.cloud_image)
-  instance_type     = lookup(each.value, "instance_type", local.default_worker_instance_type)
-  storage_size      = lookup(each.value, "storage_size", var.storage_size)
+  instance_type     = lookup(each.value, "instance_type", local.instance_type)
+  storage_size      = lookup(each.value, "storage_size", local.storage_size)
   hostpci_id        = lookup(each.value, "hostpci_id", local.hostpci_id)
 }
--- Terraform/pve/locals.tf
+++ Terraform/pve/locals.tf
@@ -1,11 +1,31 @@
 locals {
   proxmox_defaults = {
-    proxmox_endpoint  = "https://192.168.1.10:8006/"
-    node_name         = "ms-01"
-    storage_pool      = "local"
-    nvme_storage_pool = "local-lvm"
+    proxmox_endpoint = "https://192.168.1.10:8006/"
+    node_name        = "ms-01"
   }
-  minio_defaults = {
-    minio_endpoint = "https://s3.lanfordlabs.com"
+  instance_type = "m5.2large"
+  cloud_image                  = module.images.ubuntu_24_04.id
+  hostpci_id                   = null
+  storage_size                 = 40
+  ssh_public_key               = var.ssh_public_key
+  nvme_storage_pool            = var.storage_pool
+
+  cluster_nodes = {
+    master-01 = {
+      ip            = "192.168.1.60"
+      instance_type = "t3.medium"
+    }
+    worker-01 = {
+      ip         = "192.168.1.61"
+      hostpci_id = "0000:02:00"
+    }
+    worker-02 = {
+      ip         = "192.168.1.62"
+      hostpci_id = "0000:59:00"
+    }
+    worker-03 = {
+      ip         = "192.168.1.63"
+      hostpci_id = "0000:01:00"
+    }
   }
 }
